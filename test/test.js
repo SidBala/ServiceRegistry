6e0c482b3ff4c78a51f91e2eb0da65a801951c88
Sid Bala <Sid.Bala@Outlook.com>
2015-03-03 16:32:49 -0800

    Added DiscoverAll() to discovery multiple services in a single call
    DiscoverAll([ServiceA, ServiceB])
    =>
     { ServiceA:
       { name: 'ServiceA',
         ip: '192.168.1.1',
         port: '80',
         tags: [ 'Production', 'Version-1.12.2' ] },
      ServiceB:
       { name: 'ServiceB',
         ip: '192.168.1.2',
         port: '81',
         tags: [ 'Development', 'Version-9.98.7' ] } }

--- test/test.js
+++ test/test.js
@@ -1,38 +1,84 @@
-var Config = require('..');
+var Registry = require('..');
 var log = require('log-colors');
 var should = require('chai').should();
 
+var util = require('util');
+
+// Some dummy services
+var ServiceA = {
+		name: 'ServiceA',
+		ip: '192.168.1.1',
+		port: '80',
+		tags: ['Production', 'Version-1.12.2']
+	};
+
+var ServiceB = {
+		name: 'ServiceB',
+		ip: '192.168.1.2',
+		port: '81',
+		tags: ['Development', 'Version-9.98.7']
+	};
+
+
 describe('Basic', function() {
 	it('Register a service', function(done) {
-		var config = new Config();
+		var registry = new Registry();
 
-		// Some dummy service
-		var serviceName = 'MyService';
-		var serviceIp = '192.168.1.1';
-		var servicePort = '80';
-		var serviceTags = ['Production', 'Version-1.12.3'];
-
-		config.Register(serviceName, serviceIp, servicePort, serviceTags)
-			.then(function() {
-					// Read directly from etcd the set key
-					config.client.getAsync('MyService')
-					.then(function(mesg) {
-						var value = JSON.parse(mesg[0].node.value);
-
-						value.name.should.equal(serviceName);
-						value.ip.should.equal(serviceIp);
-						value.port.should.equal(servicePort);
-						value.tags.should.deep.equal(serviceTags);
-					});
-				})
-			.then(done.bind(this,null));
+		// Register the dummy service A
+		registry.Register(ServiceA.name, ServiceA.ip, ServiceA.port, ServiceA.tags)
+		// Now read directly from etcd the set key
+		.then(registry.client.getAsync.bind(registry.client, 'ServiceA'))
+		.then(function(message) {
+			var service = JSON.parse(message[0].node.value);
+			service.name.should.equal(ServiceA.name);
+			service.ip.should.equal(ServiceA.ip);
+			service.port.should.equal(ServiceA.port);
+			service.tags.should.deep.equal(ServiceA.tags);
+		})
+		.then(done.bind(this,null));
 	});
 
-	it('Discover a service', function(done) {
-		var config = new Config();
-		config.Discover('MyService')
-			.then(function(service) {
-			})
-			.then(done.bind(this, null));
+	it('Register a service and then discover it immediately after', function(done) {
+		var registry = new Registry();
+
+		// Register the dummy service B
+		registry.Register(ServiceB.name, ServiceB.ip, ServiceB.port, ServiceB.tags)
+		// Discover it immediately
+		.then(registry.Discover.bind(registry, ServiceB.name))
+		// Check that the returned service is the same
+		.then(function(service) {
+			service.name.should.equal(ServiceB.name);
+			service.ip.should.equal(ServiceB.ip);
+			service.port.should.equal(ServiceB.port);
+			service.tags.should.deep.equal(ServiceB.tags);
+		})
+		.then(done.bind(this, null));
+	});
+
+	it('Discover several services', function(done) {
+		var registry = new Registry();
+
+		// Register service A
+		registry.Register(ServiceA.name, ServiceA.ip, ServiceA.port, ServiceA.tags)
+		// Register service B
+		.then(registry.Register.bind(registry, ServiceB.name, ServiceB.ip, ServiceB.port, ServiceB.tags))
+		// Discovery both ServiceA and ServiceB
+		.then(registry.DiscoverAll.bind(registry, [ServiceA.name, ServiceB.name]))
+		.then(function(services) {
+
+			console.log(util.inspect(services));
+			// Service A
+			services.ServiceA.name.should.equal(ServiceA.name);
+			services.ServiceA.ip.should.equal(ServiceA.ip);
+			services.ServiceA.port.should.equal(ServiceA.port);
+			services.ServiceA.tags.should.deep.equal(ServiceA.tags);
+
+			// Service B
+			services.ServiceB.name.should.equal(ServiceB.name);
+			services.ServiceB.ip.should.equal(ServiceB.ip);
+			services.ServiceB.port.should.equal(ServiceB.port);
+			services.ServiceB.tags.should.deep.equal(ServiceB.tags);
+		})
+		.then(done.bind(this, null));
 	});
 });
